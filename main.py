28b91f6e9137faa709f5445abe54dc10125ddb61
김서원0727 <https://github.com/kimseowon0727/kim_dashboard.git>
2024-08-30 14:11:16 +0900

    실습

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from data_loader import load_data
 def main():
     st.title("this is a stock dashboard")
 
+    df = load_data()
+    
 if __name__=='__main__':
     main()
 
